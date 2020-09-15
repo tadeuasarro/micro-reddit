8e9b8c2e512fcd1a2f5f693082b6aa8d11143dc3
tadeuasarro <tadeutas@gmail.com>
2020-09-15 19:12:34 -0300

    Reset master

--- app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id'
-  belongs_to :post, foreign_key: 'post_id'
-  validates :user_id, presence: true
-  validates :post_id, presence: true
-  validates :content, presence: true, length: { minimum: 1, maximum: 140}
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 10 }
-end
