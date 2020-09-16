dd3a9fbc0904a4c9d14daf15b64ea5028f852d86
tadeuasarro <tadeutas@gmail.com>
2020-09-16 13:18:39 -0300

    Add linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    validates :username, uniqueness: true, presence: true, length: { in: 2..15 }
-    validates :password, presence:true, length: { in: 6..20 }
-    validates :email, uniqueness: true, presence: true
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  validates :username, uniqueness: true, presence: true, length: { in: 2..15 }
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :email, uniqueness: true, presence: true
 end
