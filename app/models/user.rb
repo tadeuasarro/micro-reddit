57104c22ca0dd6117bfb97e794b790aacef2332f
Shaqri <peter200970@hotmail.com>
2020-09-16 10:08:38 -0500

    create user model and validations

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, uniqueness: true, presence: true, length: { in: 2..15 }
+    validates :password, presence:true, length: { in: 6..20 }
+    validates :email, uniqueness: true, presence: true
+end
